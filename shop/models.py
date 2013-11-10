0b564e9c55a8955cf35349622bb5e3ae8bb704b8
kodiers <kodiers@gmail.com>
2013-11-10 06:37:57 +0400

    Changing in models
    Deleted Price model,
    Changed Orders models
    Created OrdersAdmin, OrdersItem models

--- shop/models.py
+++ shop/models.py
@@ -19,18 +19,14 @@ class CategoryGoods(models.Model):
         return self.title
 
 class CategoryGoodsAdmin(admin.ModelAdmin):
+    """Goods category admin"""
     prepopulated_fields = {'slug':('title',)}
-
-class Price(models.Model):
-    cost = models.IntegerField(default=0)
-    price = models.IntegerField(default=0)
-    sale = models.IntegerField(default=0)
-    #good_title = models.ForeignKey(Goods)
-    modified = models.DateTimeField(default=datetime.datetime.now())
+    
 
 class Goods(models.Model):
+    """ Goods model"""
     title = models.CharField(max_length=100)
-    good_price = models.ForeignKey(Price)
+    good_price = models.FloatField()
     quantity = models.IntegerField(default=0)
     image = models.ImageField(upload_to='img')
     info = models.TextField(max_length=400, blank=True)
@@ -45,25 +41,47 @@ class Goods(models.Model):
         return "%s" % self.pk
 
 class GoodsAdmin(admin.ModelAdmin):
+    """Goods admin model"""
     list_display = ('title', 'good_price', 'quantity', 'active', 'modified')
     list_filter = ('title', 'good_price', 'quantity', 'active', 'modified')
     search_fields = ('title', 'info', 'good_price')
 
+
+class OrderItem(models.Model):
+    """Order item model"""
+    goods = models.ForeignKey(Goods, related_name="+")
+    unit_price = models.ForeignKey(Goods, related_name="+")
+    quantity = models.IntegerField()
+    total_price = models.IntegerField()
+
 class Orders(models.Model):
-    # TODO: create multiple goods in order for user
-    # TODO: change Orders model and create OrdersAdmin model
+    """Orders model"""
     ORDER_STATUS = (
         (0, 'Received'),
         (1, 'In Progress'),
         (2, 'Complete'),
     )
     owner = models.ForeignKey(User)
-    #goods = models.ForeignKey(Goods)
-    quantity = models.IntegerField(default=0)
-    #cost = quantity * goods.good_price
-    date = models.DateTimeField(default=datetime.datetime.now())
+    items = models.ManyToManyField(OrderItem, related_name="+")
+    total_cost = models.ForeignKey(OrderItem, related_name="+")
+    date_created = models.DateTimeField(default=datetime.datetime.now())
+    date_modified = models.DateTimeField(default=datetime.datetime.now())
+    date_closed = models.DateTimeField(blank=True)
+    status = models.CharField(choices=ORDER_STATUS, default=0, max_length=12)
+
+    class Meta:
+        verbose_name_plural="Orders"
+        ordering = ['-date_created']
+
+
+class OrdersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'owner', 'total_cost', 'date_created', 'date_modified', 'date_closed', 'status')
+    list_filter = ('id', 'owner', 'total_cost', 'date_created', 'date_modified', 'date_closed', 'status')
+    search_fields = ('owner', 'goods', 'quantity', 'cost')
+
 
 admin.site.register(CategoryGoods, CategoryGoodsAdmin)
 admin.site.register(Goods, GoodsAdmin)
+admin.site.register(Orders, OrdersAdmin)
 
 
